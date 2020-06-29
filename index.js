5dba73816eb30e5d80e8cd8a680b3507deec7da2
Tenzing Shrestha <tenzingtheking85@gmail.com>
2020-06-29 14:59:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,6 @@ function happyHolidaysTo(name) {
 function happyCustomHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`;
 }
+function holidayCountdown(holiday, days) {
+  return `It's ${days} to ${holiday}!`;
+}
